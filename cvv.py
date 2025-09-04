544de4fcf09ebb300f491717cc6d6858e9241580
IbrokhimN <161945489+IbrokhimN@users.noreply.github.com>
2025-09-05 00:00:48 +0500

    Update cvv.py

--- cvv.py
+++ cvv.py
@@ -1,11 +1,18 @@
 import sys
+import os
 import cv2
-import mediapipe as mp
 import math
+import time
+import mediapipe as mp
+from collections import deque
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import QTimer
+
+os.environ['QT_QPA_PLATFORM'] = 'wayland'
+
 screen_w, screen_h = 1920, 1080
+CLICK_THRESHOLD = 0.04
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.5)
@@ -15,10 +22,8 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 960)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-CLICK_THRESHOLD = 0.04
-
 def distance(p1, p2):
-    return math.hypot(p2[0]-p1[0], p2[1]-p1[1])
+    return math.hypot(p2[0] - p1[0], p2[1] - p1[1])
 
 class CameraWidget(QWidget):
     def __init__(self):
@@ -28,12 +33,21 @@ class CameraWidget(QWidget):
         layout = QVBoxLayout()
         layout.addWidget(self.label)
         self.setLayout(layout)
+
         self.gesture_text = ""
-        self.cursor_pos = (0,0)
+        self.cursor_pos = (0, 0)
+
+        self.gesture_history = deque(maxlen=5) 
+        self.last_time = time.time()
+        self.fps = 0
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_frame)
         self.timer.start(30)
+
+    def get_stable_gesture(self, gesture):
+        self.gesture_history.append(gesture)
+        return max(set(self.gesture_history), key=self.gesture_history.count)
 
     def update_frame(self):
         ret, frame = cap.read()
@@ -45,36 +59,47 @@ class CameraWidget(QWidget):
         result = hands.process(rgb)
 
         h, w, _ = frame.shape
-        self.gesture_text = ""
+        gesture = "None"
 
         if result.multi_hand_landmarks:
             for hand_landmarks in result.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-                ix = hand_landmarks.landmark[8].x
-                iy = hand_landmarks.landmark[8].y
-                self.cursor_pos = (int(ix*screen_w), int(iy*screen_h))
 
-                tx = hand_landmarks.landmark[4].x
-                ty = hand_landmarks.landmark[4].y
-                mx = hand_landmarks.landmark[12].x
-                my = hand_landmarks.landmark[12].y
+                ix, iy = hand_landmarks.landmark[8].x, hand_landmarks.landmark[8].y
+                tx, ty = hand_landmarks.landmark[4].x, hand_landmarks.landmark[4].y
+                mx, my = hand_landmarks.landmark[12].x, hand_landmarks.landmark[12].y
+
+                self.cursor_pos = (int(ix * screen_w), int(iy * screen_h))
+
                 if distance((ix, iy), (tx, ty)) < CLICK_THRESHOLD:
-                    self.gesture_text = "Left Click (visual)"
+                    gesture = "Left Click"
                 elif distance((ix, iy), (mx, my)) < CLICK_THRESHOLD:
-                    self.gesture_text = "Right Click (visual)"
+                    gesture = "Right Click"
+                elif distance((ix, iy), (mx, my)) < 0.02:
+                    gesture = "Double Click"
                 else:
-                    finger_dist = distance((ix, iy), (mx, my))
-                    if finger_dist < 0.02:
-                        self.gesture_text = "Double Click (visual)"
-                    else:
-                        self.gesture_text = "Move Cursor"
-        cv2.circle(frame, (int(self.cursor_pos[0]*w/screen_w), int(self.cursor_pos[1]*h/screen_h)), 15, (0,255,0), -1)
-        cv2.putText(frame, self.gesture_text, (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
-        bytes_per_line = frame.shape[1] * frame.shape[2]
-        qimg = QImage(frame.data, w, h, bytes_per_line, QImage.Format_BGR888)
+                    gesture = "Move Cursor"
+
+        stable_gesture = self.get_stable_gesture(gesture)
+        self.gesture_text = stable_gesture
+        new_time = time.time()
+        self.fps = int(1 / (new_time - self.last_time))
+        self.last_time = new_time
+        color_map = {
+            "Left Click": (0, 255, 0),
+            "Right Click": (0, 0, 255),
+            "Double Click": (255, 0, 0),
+            "Move Cursor": (0, 255, 255),
+            "None": (200, 200, 200),
+        }
+        color = color_map.get(stable_gesture, (255, 255, 255))
+
+        cv2.circle(frame, (int(self.cursor_pos[0] * w / screen_w), int(self.cursor_pos[1] * h / screen_h)), 15, color, -1)
+        cv2.putText(frame, f"{self.gesture_text}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+        cv2.putText(frame, f"FPS: {self.fps}", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+        qimg = QImage(frame.data, w, h, frame.shape[1] * frame.shape[2], QImage.Format_BGR888)
         self.label.setPixmap(QPixmap.fromImage(qimg))
-import os
-os.environ['QT_QPA_PLATFORM'] = 'wayland'
 
 app = QApplication(sys.argv)
 window = CameraWidget()

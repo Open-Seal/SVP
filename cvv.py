4f6cf17d330439d5b8de6f1410f77f1b0c9c1448
IbrokhimN <161945489+IbrokhimN@users.noreply.github.com>
2025-09-18 00:01:45 +0500

    Update cvv.py

--- cvv.py
+++ cvv.py
@@ -11,18 +11,14 @@ from PyQt5.QtCore import QTimer
 
 os.environ['QT_QPA_PLATFORM'] = 'wayland'
 
-screen_w, screen_h = 1920, 1080
-CLICK_THRESHOLD = 0.04
+SCREEN_W, SCREEN_H = 1920, 1080
+CLICK_THRESHOLD, DOUBLE_CLICK_THRESHOLD = 0.04, 0.02
 
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(
-    max_num_hands=1,
-    min_detection_confidence=0.7,
-    min_tracking_confidence=0.5
-)
+hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.5)
 mp_drawing = mp.solutions.drawing_utils
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 960)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
@@ -33,22 +29,30 @@ class CameraWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Hand Gesture Visualizer (Wayland)")
-        self.label = QLabel()
-        layout = QVBoxLayout()
+        self.label = QLabel(alignment=0x84)
+        layout = QVBoxLayout(self)
         layout.addWidget(self.label)
-        self.setLayout(layout)
-        self.gesture_text = ""
-        self.cursor_pos = (0, 0)
-        self.gesture_history = deque(maxlen=5) 
-        self.last_time = time.time()
-        self.fps = 0
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.update_frame)
+        self.gesture_history, self.cursor_pos = deque(maxlen=5), (0, 0)
+        self.gesture_text, self.last_time, self.fps = "", time.time(), 0
+        self.timer = QTimer(self, timeout=self.update_frame)
         self.timer.start(30)
 
     def get_stable_gesture(self, gesture):
         self.gesture_history.append(gesture)
         return max(set(self.gesture_history), key=self.gesture_history.count)
+
+    def detect_gesture(self, landmarks):
+        ix, iy = landmarks[8].x, landmarks[8].y
+        tx, ty = landmarks[4].x, landmarks[4].y
+        mx, my = landmarks[12].x, landmarks[12].y
+        self.cursor_pos = (int(ix * SCREEN_W), int(iy * SCREEN_H))
+        if distance((ix, iy), (tx, ty)) < CLICK_THRESHOLD:
+            return "Left Click"
+        if distance((ix, iy), (mx, my)) < DOUBLE_CLICK_THRESHOLD:
+            return "Double Click"
+        if distance((ix, iy), (mx, my)) < CLICK_THRESHOLD:
+            return "Right Click"
+        return "Move Cursor"
 
     def update_frame(self):
         ret, frame = cap.read()
@@ -60,25 +64,14 @@ class CameraWidget(QWidget):
         h, w, _ = frame.shape
         gesture = "None"
         if result.multi_hand_landmarks:
-            for hand_landmarks in result.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-                ix, iy = hand_landmarks.landmark[8].x, hand_landmarks.landmark[8].y
-                tx, ty = hand_landmarks.landmark[4].x, hand_landmarks.landmark[4].y
-                mx, my = hand_landmarks.landmark[12].x, hand_landmarks.landmark[12].y
-                self.cursor_pos = (int(ix * screen_w), int(iy * screen_h))
-                if distance((ix, iy), (tx, ty)) < CLICK_THRESHOLD:
-                    gesture = "Left Click"
-                elif distance((ix, iy), (mx, my)) < CLICK_THRESHOLD:
-                    gesture = "Right Click"
-                elif distance((ix, iy), (mx, my)) < 0.02:
-                    gesture = "Double Click"
-                else:
-                    gesture = "Move Cursor"
+            for lm in result.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(frame, lm, mp_hands.HAND_CONNECTIONS)
+                gesture = self.detect_gesture(lm.landmark)
         stable_gesture = self.get_stable_gesture(gesture)
         self.gesture_text = stable_gesture
-        new_time = time.time()
-        self.fps = int(1 / (new_time - self.last_time))
-        self.last_time = new_time
+        now = time.time()
+        self.fps = int(1 / (now - self.last_time))
+        self.last_time = now
         color_map = {
             "Left Click": (0, 255, 0),
             "Right Click": (0, 0, 255),
@@ -87,10 +80,10 @@ class CameraWidget(QWidget):
             "None": (200, 200, 200),
         }
         color = color_map.get(stable_gesture, (255, 255, 255))
-        cv2.circle(frame, (int(self.cursor_pos[0] * w / screen_w), int(self.cursor_pos[1] * h / screen_h)), 15, color, -1)
-        cv2.putText(frame, f"{self.gesture_text}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+        cv2.circle(frame, (int(self.cursor_pos[0] * w / SCREEN_W), int(self.cursor_pos[1] * h / SCREEN_H)), 15, color, -1)
+        cv2.putText(frame, self.gesture_text, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
         cv2.putText(frame, f"FPS: {self.fps}", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-        qimg = QImage(frame.data, w, h, frame.shape[1] * frame.shape[2], QImage.Format_BGR888)
+        qimg = QImage(frame.data, w, h, frame.strides[0], QImage.Format_BGR888)
         self.label.setPixmap(QPixmap.fromImage(qimg))
 
 app = QApplication(sys.argv)
